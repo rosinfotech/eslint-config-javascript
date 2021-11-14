1f67929bdc7b067638ba3b3c3198374a324e9cd1
rosinfotech <rosinfo.tech@gmail.com>
2021-11-14 16:08:05 +0300

    0.0.17

--- index.js
+++ index.js
@@ -99,6 +99,101 @@ module.exports = {
             1,
             "beside"
         ],
+        "import/default": 0,
+        "import/dynamic-import-chunkname": 0,
+        "import/export": 2,
+        "import/exports-last": 0,
+        "import/extensions": 0,
+        "import/first": 0,
+        "import/group-exports": 0,
+        "import/max-dependencies": 0,
+        "import/named": 0,
+        "import/namespace": 2,
+        "import/newline-after-import": 2,
+        "import/no-absolute-path": 2,
+        "import/no-amd": 2,
+        "import/no-anonymous-default-export": [
+            1,
+            {
+                "allowAnonymousClass": false,
+                "allowAnonymousFunction": false,
+                "allowArray": false,
+                "allowArrowFunction": false,
+                "allowCallExpression": true,
+                "allowLiteral": false,
+                "allowObject": false
+            }
+        ],
+        "import/no-commonjs": [
+            1,
+            {
+                "allowRequire": true
+            }
+        ],
+        "import/no-cycle": 2,
+        "import/no-default-export": 0,
+        "import/no-deprecated": 2,
+        "import/no-duplicates": 2,
+        "import/no-dynamic-require": 2,
+        "import/no-extraneous-dependencies": [
+            1,
+            {
+                "devDependencies": false,
+                "optionalDependencies": false,
+                "peerDependencies": false
+            }
+        ],
+        "import/no-import-module-exports": 0,
+        "import/no-internal-modules": 0,
+        "import/no-mutable-exports": 2,
+        "import/no-named-as-default": 2,
+        "import/no-named-as-default-member": 2,
+        "import/no-named-default": 0,
+        "import/no-named-export": 0,
+        "import/no-nodejs-modules": 0,
+        "import/no-relative-packages": 0,
+        "import/no-relative-parent-imports": 0,
+        "import/no-restricted-paths": 0,
+        "import/no-self-import": 2,
+        "import/no-unassigned-import": 0,
+        "import/no-unresolved": 0,
+        "import/no-unused-modules": 2,
+        "import/no-useless-path-segments": 2,
+        "import/no-webpack-loader-syntax": 2,
+        "import/order": [
+            2,
+            {
+                "groups": [
+                    "builtin",
+                    "external",
+                    "internal",
+                    "unknown",
+                    "parent",
+                    "sibling",
+                    "index",
+                    "object",
+                    "type"
+                ],
+                "newlines-between": "never",
+                "pathGroups": [
+                    {
+                        "group": "builtin",
+                        "pattern": "react",
+                        "position": "before"
+                    },
+                    {
+                        "group": "external",
+                        "pattern": "@**",
+                        "position": "after"
+                    }
+                ],
+                "pathGroupsExcludedImportTypes": [
+                    "builtin"
+                ]
+            }
+        ],
+        "import/prefer-default-export": 0,
+        "import/unambiguous": 2,        
         "indent": [
             1,
             4,
@@ -155,7 +250,7 @@ module.exports = {
         "multiline-comment-style": 0,
         "new-parens": 1,
         "newline-per-chained-call": 0,
-        "no-confusing-arrow": 1,
+        "no-confusing-arrow": 0,
         "no-else-return": [
             1,
             {
@@ -238,7 +333,18 @@ module.exports = {
         "object-curly-newline": [
             1,
             {
-                "multiline": true
+                "ExportDeclaration": {
+                    "multiline": true
+                },
+                "ImportDeclaration": {
+                    "multiline": true
+                },
+                "ObjectExpression": {
+                    "multiline": true
+                },
+                "ObjectPattern": {
+                    "multiline": true
+                }
             }
         ],
         "object-curly-spacing": [
@@ -252,7 +358,7 @@ module.exports = {
         "object-property-newline": [
             1,
             {
-                "allowAllPropertiesOnSameLine": true
+                "allowAllPropertiesOnSameLine": false
             }
         ],
         "object-shorthand": [
@@ -274,7 +380,13 @@ module.exports = {
         ],
         "operator-linebreak": [
             1,
-            "after"
+            "after",
+            {
+                "overrides": {
+                    ":": "before",
+                    "?": "before"
+                }
+            }
         ],
         "padded-blocks": [
             1,
